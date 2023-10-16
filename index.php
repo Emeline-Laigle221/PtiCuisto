fa422e8eada0adfb82886eeaf8d126c01f7ab2da
Maxime <maxime.jobard@etu.unicaen.fr>
2023-10-16 15:37:49 +0200

    build : Début structure Edito

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PtiCuisto</title>
+</head>
+<body>
+    <header>
+        <img src="img/Logo.png" alt="Logo">
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="">Nos Recettes</a>
+            <a href="">Filtres</a>
+            <a href="">Connexion</a>
+        </nav>
+    </header>
+    <main>
+        <?php include 'template/Edito.php'; ?>
+    </main>
+    <footer>
+        <div>
+            <img src="" alt="Facebook">
+            <img src="" alt="X">
+            <img src="" alt="Indeed">
+        </div>
+    </footer>
+</body>
+</html>
---
+++ template/Edito.php
@@ -0,0 +1,22 @@
+<section>
+    <img src="" alt="">
+    <div id="recette">
+        <h1>LES DERNIÈRES RECETTES</h1>
+        <div>
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate. Doloribus, dicta iste quam saepe nam et cum. Explicabo tenetur laborum, sint beatae aliquam incidunt impedit ratione quasi earum autem.</p>
+        </div>
+        <div>
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate. Doloribus, dicta iste quam saepe nam et cum. Explicabo tenetur laborum, sint beatae aliquam incidunt impedit ratione quasi earum autem.</p>
+        </div>
+        <div>
+            <img src="" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate. Doloribus, dicta iste quam saepe nam et cum. Explicabo tenetur laborum, sint beatae aliquam incidunt impedit ratione quasi earum autem.</p>
+        </div>
+    </div>
+    <div>
+        <img src="img/Pticuisto.png" alt="Pticuisto">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nulla quasi animi distinctio dolor doloremque aspernatur quisquam a aut consequatur! Perferendis natus ipsum reiciendis animi adipisci provident nesciunt eveniet ipsa.</p>
+    </div>
+</section>
