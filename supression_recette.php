7e46f00a95e4d4454a1d41e27f44789fef1243f7
emeline <emeline.laigle@unicaen.fr>
2023-10-22 12:29:59 +0200

    feat:debut_fonctionnalité

--- supression_recette.php
+++ supression_recette.php
@@ -1,8 +1,11 @@
 <?php
-if(isset($_SESSION['pseudo']) && isset($_POST['message'])){
-    $req = $bdd->prepare('DELETE FROM recette where  ');
-    $req->execute(array(($_SESSION['pseudo']), strip_tags($_POST['message']))); // idem pour la suppression des balises éventuelles du message 
-    //strip_tags enleve balise html si il y en a.
-    //isset regarde si la variable n'est pas vide.
-}
+    if(isset($_SESSION['pseudo']) && isset($_POST['message'])){
+        $req = $bdd->prepare('DELETE FROM recette where  ');
+        $req->execute(array(($_SESSION['pseudo']), strip_tags($_POST['message']))); // idem pour la suppression des balises éventuelles du message 
+        
+            $req = $bdd->prepare('INSERT INTO recette (recette_id, titre,resume,image,date_creation,date_modification,contenu,validation,categorie_id,uti_id) VALUES(?,?,?,?,NOW(),NOW(),?,0,?,?)');
+            $req->execute(array(2, strip_tags($_POST['titre_recette']),strip_tags($_POST['resume_de_la_recette']),strip_tags($_POST['lien_image']),strip_tags($_POST['contenu_de_la_recette']),...,$_SESSION['id'])); 
+    }
+
+    //header('Location : ajout_rectte.php');
 ?>
