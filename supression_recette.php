4058994b67f0627ca2699348ab5b7c6ac3186ce9
emeline <emeline.laigle@unicaen.fr>
2023-10-23 17:47:45 +0200

    feat: creation_fonction_supression

--- supression_recette.php
+++ supression_recette.php
@@ -1,10 +1,13 @@
 <?php
-    if(isset(/**/ ){
-        $req = $bdd->prepare('DELETE FROM recette where  ');
-        $req->execute(array(($_SESSION['pseudo']), strip_tags($_POST['message']))); // idem pour la suppression des balises éventuelles du message 
-        
-            $req = $bdd->prepare('INSERT INTO recette (recette_id, titre,resume,image,date_creation,date_modification,contenu,validation,categorie_id,uti_id) VALUES(?,?,?,?,NOW(),NOW(),?,0,?,?)');
-            $req->execute(array(2, strip_tags($_POST['titre_recette']),strip_tags($_POST['resume_de_la_recette']),strip_tags($_POST['lien_image']),strip_tags($_POST['contenu_de_la_recette']),...,$_SESSION['id'])); 
+    include_once "connexion.php";
+
+    function supression(int $recID){
+        if(isset($recID)){
+            $req3 = $bdd->prepare('DELETE FROM `CONTENIR` WHERE REC_ID=?');
+            $req3->execute(array($recID)); 
+            $req4 = $bdd->prepare('DELETE FROM `RECETTE` WHERE REC_ID=?');
+            $req4->execute(array($recID)); 
+        }
     }
 
     //header('Location : ajout_rectte.php');
