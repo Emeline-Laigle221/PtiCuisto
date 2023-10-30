686b2dda27f1b34684b38bea476f4f5410bad820
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-10-30 14:22:01 +0100

    feat: fonction afficher_liste terminée

--- controller/RecetteController.php
+++ controller/RecetteController.php
@@ -1,18 +1,14 @@
 <?php
         require_once('../modele/RecetteManager.php');
 
-        function afficher_liste(){
+        function afficher_liste($depart){
             $recettes = liste();
-
             echo '<table>';
-            for($i = 0; $i < 10 && $i < count($recettes); $i++){ 
-                echo '<tr> <td>' . $recettes[$i]['titre'] . '<td/><td>' . $recettes[$i]['rec_resume']  . '<td/><tr/>';
+            for($i = $depart; $i < $depart + 10 && $i < count($recettes); $i++){ 
+                echo '<tr> <td>' . $recettes[$i]['titre'] . '<td/><td>' . $recettes[$i]['categorie'] . '<td/><td>' . $recettes[$i]['rec_resume']  . '<td/>' . '<td>' . '<img src=' . $recettes[$i]['rec_image'] . 'alt="image de la recette">' . '<td/><tr/>';
                 echo '<br>';
             }
             echo '<table/>';
         }
-
-        afficher_liste();
-
 
 ?>
--- modele/RecetteManager.php
+++ modele/RecetteManager.php
@@ -1,7 +1,7 @@
 <?php
     function liste(){
         include_once("../template/connexion.php");
-        $reponse = $bdd->query('SELECT titre, rec_resume from RECETTE ORDER BY date_modification;');
+        $reponse = $bdd->query('SELECT titre, rec_resume, rec_image, cat_intitule as categorie from RECETTE join CATEGORIE using(categorie_id) ORDER BY date_modification;');
 
         $retour = array();
         $i = 0;
@@ -9,6 +9,9 @@
         while ($donnees = $reponse->fetch()){ 
             $retour[$i]['titre'] = $donnees['titre'];
             $retour[$i]['rec_resume'] = $donnees['rec_resume'];
+            $retour[$i]['rec_image'] = $donnees['rec_image'];
+            $retour[$i]['categorie'] = $donnees['categorie'];
+            $i++;
         }
 
         return $retour;
