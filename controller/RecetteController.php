c4a3454d92261755985a0c2ae76f11c9519e6a9b
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-10-31 12:37:49 +0100

    feat: Bouton "Plus" pour afficher les 10 suivantes

--- controller/RecetteController.php
+++ controller/RecetteController.php
@@ -1,14 +1,18 @@
 <?php
-        require_once('../modele/RecetteManager.php');
+    require_once('modele/RecetteManager.php'); //Récupération du modèle
 
-        function afficher_liste($depart){
-            $recettes = liste();
-            echo '<table>';
-            for($i = $depart; $i < $depart + 10 && $i < count($recettes); $i++){ 
-                echo '<tr> <td>' . $recettes[$i]['titre'] . '<td/><td>' . $recettes[$i]['categorie'] . '<td/><td>' . $recettes[$i]['rec_resume']  . '<td/>' . '<td>' . '<img src=' . $recettes[$i]['rec_image'] . 'alt="image de la recette">' . '<td/><tr/>';
-                echo '<br>';
-            }
-            echo '<table/>';
+    /**
+     * Fonction pour la page "Nos recettes"
+     * le paramère $depart indique où on en est de la liste (utilisation du bouton "Plus" pour avancer de 10 en 10)
+     */
+    function afficher_liste($depart){
+        $recettes = liste();
+        echo '<table>';
+        for($i = $depart; $i < $depart + 10 && $i < count($recettes); $i++){ 
+            echo '<tr> <td>' . $recettes[$i]['titre'] . '<td/><td>' . $recettes[$i]['categorie'] . '<td/><td>' . $recettes[$i]['rec_resume']  . '<td/>' . '<td>' . '<img src=' . $recettes[$i]['rec_image'] . 'alt="image de la recette">' . '<td/><tr/>';
+            echo '<br>';
         }
-
+        echo '<table/>';
+        echo '<a href="index.php?plus=' . $depart . '">Plus</a>';
+    }
 ?>
--- modele/RecetteManager.php
+++ modele/RecetteManager.php
@@ -1,6 +1,13 @@
 <?php
+    
+
+    /**
+     * Fonction retournant toutes les recettes avec les propriétés : titre, résumé, image et intitulé de la catégorie
+     * Le retour se fait sous la forme d'un tableau à deux dimensions : la première pour choisir la recette, et la deuxième pourchoisir la propriété (titre, rec_resume, 
+     * rec_image et categorie)
+     */
     function liste(){
-        include_once("../template/connexion.php");
+        include_once("template/connexion.php"); //Ligne à modifier avec le .env
         $reponse = $bdd->query('SELECT titre, rec_resume, rec_image, cat_intitule as categorie from RECETTE join CATEGORIE using(categorie_id) ORDER BY date_modification;');
 
         $retour = array();
