dc134a5aed7a8c86bf6a78d9f74ef27dafd817b2
Maxime <maxime.jobard@etu.unicaen.fr>
2023-11-01 12:35:01 +0100

    refactor : enregistrement filtre dans localStorage

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ const ingredientContainer = document.querySelector(".ingredient-container");
 const categorieTriggers = document.querySelectorAll(".categorie");
 const titreTriggers = document.querySelectorAll(".titre");
 const ingredientTriggers = document.querySelectorAll(".ingredient");
+const entree = document.getElementById("entree");
+const plats = document.getElementById("plats");
+const dessert = document.getElementById("dessert");
+const VCategorie = document.getElementById("valider-categorie");
+const titre = document.getElementById("titre");
+const VTitre = document.getElementById("valider-titre");
+const VIngredient = document.getElementById("valider-ingredients");
 
 categorieTriggers.forEach(trigger =>
     trigger.addEventListener("click", ()=>{
@@ -22,3 +29,14 @@ ingredientTriggers.forEach(trigger =>
         ingredientContainer.classList.toggle("active");
     })
 );
+
+VCategorie.addEventListener("click", ()=>{
+    localStorage.setItem('entree', entree.checked);
+    localStorage.setItem('plats', plats.checked);
+    localStorage.setItem('dessert', dessert.checked);
+});
+
+VTitre.addEventListener("click", ()=>{
+    localStorage.setItem('titre', titre.value);
+});
+
