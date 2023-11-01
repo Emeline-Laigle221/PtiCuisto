e58037d6988170b66e92d6ae96361198648accc9
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-11-01 15:44:31 +0100

    feat: affichage des ingrédients avec les quantités

--- controller/RecetteController.php
+++ controller/RecetteController.php
@@ -7,7 +7,18 @@
         echo '<img src=' . $recette['rec_image'] . 'alt="image de la recette">';
         echo '<p>' . $recette['categorie'] . '</p>';
         echo '<p>' . $recette['rec_resume'] . '</p>';
+
+        $ingredients = lister_ingredients(10);
+
+        echo '<table>';
+        for($i = 0; $i < count($ingredients); $i++){ 
+            echo '<tr> <td>' . $ingredients[$i]['quantite'] . ' g <td/><td>' . $ingredients[$i]['intitule'] . '<td/><td>' . $ingredients[$i]['description']  . '<td/><tr/>';
+            echo '<br>';
+        }
+        echo '<table/>';
+
         echo '<p>' . $recette['contenu'] . '</p>';
+
     }
     
 ?>
--- modele/RecetteManager.php
+++ modele/RecetteManager.php
@@ -1,8 +1,9 @@
 <?php
+    
     function trouver_recette($nom){
-        include_once("template/connexion.php"); //Ligne à modifier avec le .env
+        require("template/connexion.php"); //Ligne à modifier avec le .env
 
-        $reponse = $bdd->query("SELECT titre, rec_resume, rec_image, cat_intitule, contenu from RECETTE join CATEGORIE using(categorie_id) where titre = '" . $nom . "';");
+        $reponse = $bdd->query("SELECT rec_id, titre, rec_resume, rec_image, cat_intitule, contenu from RECETTE join CATEGORIE using(categorie_id) where titre = '" . $nom . "';");
 
         $retour = array();
 
@@ -12,9 +13,27 @@
             $retour['rec_image'] = $donnees['rec_image'];
             $retour['categorie'] = $donnees['cat_intitule'];
             $retour['contenu'] = $donnees['contenu'];
+            $retour['rec_id'] = $donnees['rec_id'];
         }
 
         return $retour;
 
+    }
+
+    function lister_ingredients($id){
+        require("template/connexion.php"); //Ligne à modifier avec le .env
+        $reponse = $bdd->query("SELECT quantite, ING_INTITULE as intitule, ING_DESCRIPTION as descritpion FROM CONTENIR join INGREDIENT using(INGREDIENT_ID) where REC_ID = ". $id ."; ");
+
+        $i = 0;
+        $retour = array();
+
+        while ($donnees = $reponse->fetch()){ 
+            $retour[$i]['quantite'] = $donnees['quantite'];
+            $retour[$i]['intitule'] = $donnees['intitule'];
+            $retour[$i]['description'] = $donnees['descritpion'];
+            $i++;
+        }
+
+        return $retour;
     }
 ?>
