6a5bb3fb29aa2bf72820e953e71cad0cfab542f8
emeline <emeline.laigle@unicaen.fr>
2023-11-10 22:44:50 +0100

    fix: amélioration avec supression ingredient

--- supression_recette.php
+++ supression_recette.php
@@ -4,14 +4,17 @@
     function supression(int $recID){
         include_once "connexion.php";
         if(isset($recID)){
-            $req3 = $bdd->prepare('DELETE FROM `CONTENIR` WHERE REC_ID=?');
+            $req3 = $bdd->prepare('DELETE FROM `CONTENIR` WHERE REC_ID=?'); //supprime les lignes dans contenir qui lie des ingrédients à la recette
             $req3->execute(array($recID)); 
-            $req4 = $bdd->prepare('DELETE FROM `RECETTE` WHERE REC_ID=?');
+            $req4 = $bdd->prepare('DELETE FROM `RECETTE` WHERE REC_ID=?');//supprime la recette
             $req4->execute(array($recID)); 
+            $req5 = $bdd->prepare('DELETE FROM `INGREDIENT` WHERE INGREDIENT_ID NOT IN ( SELECT INGREDIENT_ID FROM `CONTENIR`)');//supprime les ingrédients qui ne sont plus lier à aucune recette
+            $req5->execute(array()); 
         }
     }
 
-    supression(24);
+    //exemple appel de la fonction 
+    //supression(28);
 
     //header('Location : ajout_rectte.php');
 ?>
