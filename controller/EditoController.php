51d8022966418d49f74349f55bc6c6fd12d5974c
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-11-10 14:33:19 +0100

    fix: la saisie ne peut plus être vide

--- controller/EditoController.php
+++ controller/EditoController.php
@@ -2,6 +2,10 @@
     require_once('modele/EditoManager.php'); //Récupération du modèle
     
     function traiter_formulaire_edito(){
-        ajouter_edito(strip_tags($_POST['contenu']));
+        if($_POST['contenu'].length == 0){
+            echo 'La zone de texte est vide.';
+        }else{
+            ajouter_edito(strip_tags($_POST['contenu']));
+        }
     }
 ?>
--- template/formulaire_edito.php
+++ template/formulaire_edito.php
@@ -1,20 +1,25 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Éditio de l'édito</title>
-</head>
-<body>
-    <?php
-        if(isset($_POST['contenu'])){
-            require_once("controller/EditoController.php");
-            traiter_formulaire_edito();
+
+<?php
+    if(isset($_POST['contenu'])){
+        require_once("controller/EditoController.php");
+        traiter_formulaire_edito();
+    }
+?>
+
+<form action="index.php?page=modifier_edito" method="post">
+    <input type="text" name="contenu" id="zone_texte" />
+    <button type="submit" id="bouton" disabled>Enregistrer</button>
+</form>
+<script>
+    const entree = document.getElementById("zone_texte");
+    const bouton = document.getElementById("bouton");
+
+    entree.addEventListener('change',()=>{
+        if(entree.value.length > 200 || entree.value.length == 0){
+            bouton.disabled = true;
+        }else{
+            bouton.disabled = false;
         }
-    ?>
-    <form action="index.php?page=modifier_edito" method="post">
-        <input type="text" name="contenu">
-        <button type="submit">Enregistrer</button>
-    </form>
-</body>
-</html>
+    });
+
+</script>
